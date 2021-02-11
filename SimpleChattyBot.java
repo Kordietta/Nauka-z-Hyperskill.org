eaa5816e663718311053f787f7aa02ca4eab2c48
Kordietta <78843822+Kordietta@users.noreply.github.com>
2021-02-11 11:05:13 +0100

    Add files via upload

---
+++ SimpleChattyBot.java
@@ -0,0 +1,9 @@
+public class SimpleChattyBot {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello! My name is Robbie.");
+
+        System.out.println("Nice to see you :)");
+    }
+}
