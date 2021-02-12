6d80c7e6bf96230b24b911e5d5e88e7850c4e288
Kordietta <78843822+Kordietta@users.noreply.github.com>
2021-02-12 08:43:27 +0100

    Wprowadzenie imienia użytkownika

--- SimpleChattyBot.java
+++ SimpleChattyBot.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class SimpleChattyBot {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello! My name is Robbie.");
+        System.out.println("Hello! My name is Robbie. ");
 
-        System.out.println("Nice to see you :)");
+        System.out.println("Please, remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userName = scanner.next();  
+
+        System.out.println("What a great name you have, " + userName +"!");
     }
 }
