4716d6fda7d6dbc9658cec871861cedebf45ae44
Kordietta <78843822+Kordietta@users.noreply.github.com>
2021-02-12 09:05:23 +0100

    Formuła zwracająca wiek użytkownika

--- SimpleChattyBot.java
+++ SimpleChattyBot.java
@@ -13,5 +13,17 @@ public class SimpleChattyBot {
         String userName = scanner.next();  
 
         System.out.println("What a great name you have, " + userName +"!");
+
+        System.out.println("Let me guess your age.");
+
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println(userName + ", your age is " + age + "!");
     }
 }
