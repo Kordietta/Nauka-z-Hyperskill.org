a708472165c89878d0a34700aab8d75e02fe5c59
Kordietta <78843822+Kordietta@users.noreply.github.com>
2021-02-14 14:54:51 +0100

    Estimate the number of servings

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -3,27 +3,43 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
-        System.out.println();
+   
+        int requiredWater = 200; 
+        int requiredMilk = 50;
+        int requiredBeans = 15;
         
-        System.out.println("Write how many cups of coffee you will need:");
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int beans = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
+        int order = scanner.nextInt();
         
-        int amountCups = scanner.nextInt();
+        int cupsOfWater = water / requiredWater;
+        int cupsOfMilk = milk / requiredMilk;
+        int cupsOfBeans = beans / requiredBeans;
         
-        int water = 200; 
-        int milk = 50;
-        int beans = 15;
+        int cups = 0;
         
-        System.out.println("For " + amountCups +" cups of coffee you will need:");
-        System.out.println(amountCups*water + " ml of water");
-        System.out.println(amountCups*milk + " ml of milk");
-        System.out.println(amountCups*beans +" g of coffee beans");
+        if (cupsOfWater <= cupsOfMilk && cupsOfWater <= cupsOfBeans) {
+            cups = cupsOfWater;    
+        } else
+        if (cupsOfMilk <= cupsOfWater && cupsOfMilk <= cupsOfBeans) {
+            cups = cupsOfMilk;    
+        } else
+        if (cupsOfBeans <= cupsOfWater && cupsOfBeans <= cupsOfMilk) {
+            cups = cupsOfBeans;    
+        }
         
+        if (cups < order) {
+            System.out.println("No, I can make only " + cups + " cup(s) of coffee");
+        } else if (cups == order) {
+            System.out.println("Yes, I can make that amount of coffee ");
+        } else if (cups > order) {
+            int delta = cups - order;
+            System.out.println("Yes, I can make that amount of coffee (and even " + delta + " more than that)");
+        }
     }
 }
